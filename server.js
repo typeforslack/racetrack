3e1c98e3fc1cdee99020d9ddf2cf73fe39188745
TheMightyRaider <sivasanjay7@gmail.com>
2020-06-14 14:37:05 +0530

    Added racestart timer and constraint for user entry

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const io = require("socket.io")(server);
 
 const PORT = 4000;
 const expiryInSeconds = 3600;
+const raceStartingTime = 10;
 
 let redisClient = redis.createClient(6379, "127.0.0.1");
 const aget = promisify(redisClient.get).bind(redisClient);
@@ -38,6 +39,7 @@ app.get("/generateraceAPI", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("A user is connected");
+
   socket.on("disconnect", () => {
     redisClient.keys("*", (err, rooms) => {
       for (let i = 0; i < rooms.length; i++) {
@@ -63,55 +65,78 @@ io.on("connection", (socket) => {
   socket.on("create/join", async ({ room, username }) => {
     /*  
         {
-          'room1':{'user1:'name','user2':'name'},
+          'room1':8ms,[{'user1:'name'},{'user2':'name'}],
           'room2':{'user3':'name','user4':'name'},
         } 
     */
+    userExistsAlready = io.sockets.adapter.sids[socket.id][room];
+    if (userExistsAlready) {
+      io.to(socket.id).emit(
+        "ALREADY_JOINED",
+        "You have already joined the room!"
+      );
+    } else {
+      return aget(room)
+        .then((res) => {
+          if (res != null) {
+            res = JSON.parse(res);
+            time = Math.floor(Date.now() / 1000);
+            diff = time - res[0];
+            if (diff < raceStartingTime) {
+              userDetails = {};
+              userDetails.name = username;
+              userDetails.socketId = socket.id;
+              res.push(userDetails);
+              redisClient.set(room, JSON.stringify(res), "EX", expiryInSeconds);
+              return;
+            } else {
+              io.to(socket.id).emit("RACE_STARTED", "Race has already started");
+              return false;
+            }
+          } else {
+            time = Math.floor(Date.now() / 1000);
+            userDetails = {};
+            userDetails.name = username;
+            userDetails.socketId = socket.id;
+            redisClient.set(
+              room,
+              JSON.stringify([time, userDetails]),
+              "EX",
+              expiryInSeconds
+            );
+            return;
+          }
+        })
+        .then((userAddedInRedis = true) => {
+          if (userAddedInRedis == true) {
+            socket.join(room);
 
-    socket.join(room);
-    return aget(room)
-      .then((res) => {
-        if (res != null) {
-          res = JSON.parse(res);
-          userDetails = {};
-          userDetails.name = username;
-          userDetails.socketId = socket.id;
-          res.push(userDetails);
-          redisClient.set(room, JSON.stringify(res), "EX", expiryInSeconds);
-          return;
-        } else {
-          newRoom = {};
-          userDetails = {};
-          userDetails.name = username;
-          userDetails.socketId = socket.id;
-          redisClient.set(
-            room,
-            JSON.stringify([userDetails]),
-            "EX",
-            expiryInSeconds
-          );
-          return;
-        }
-      })
-      .then((_) => {
-        redisClient.get(room, (err, res) => {
-          res = JSON.parse(res);
-          usernames = res.map((userData) => {
-            return userData.name;
-          });
+            // io.of("/")
+            //   .in(room)
+            //   .clients((err, client) => {
+            //     console.log(client);
+            //   });
 
-          let serverData = {
-            room: room,
-            userInTheRoom: [usernames],
-          };
-          console.log("Joined");
+            redisClient.get(room, (err, res) => {
+              res = JSON.parse(res);
+              usernames = res.map((userData) => {
+                return userData.name;
+              });
 
-          io.to(room).emit("userjoined", serverData);
+              let serverData = {
+                room: room,
+                userInTheRoom: [usernames],
+              };
+              console.log("Joined");
+
+              io.to(room).emit("userjoined", serverData);
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    }
   });
 
   socket.on("Typing", ({ room, message }) => {
