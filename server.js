740100f412e990de821165b0edc210b065ea34ea
TheMightyRaider <sivasanjay7@gmail.com>
2020-06-15 19:22:52 +0530

    Added redisclient for storing user typed para and fetched para

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+const request = require("request");
 const { promisify } = require("util");
 const helper = require("./helper.js");
 
@@ -11,9 +12,14 @@ const io = require("socket.io")(server);
 const PORT = 4000;
 const expiryInSeconds = 3600;
 const raceStartingTime = 10;
+const TFSBackendURL = "http://127.0.0.1:8000/paraForRaceTrack";
 
-let redisClient = redis.createClient(6379, "127.0.0.1");
-const aget = promisify(redisClient.get).bind(redisClient);
+let raceRooms = redis.createClient(6379, "127.0.0.1");
+let paraTypedByUser = redis.createClient();
+let startRace = false;
+
+const syncraceget = promisify(raceRooms.get).bind(raceRooms);
+const syncparaget = promisify(paraTypedByUser.get).bind(paraTypedByUser);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client.html");
@@ -25,7 +31,7 @@ app.get("/index", (req, res) => {
 
 app.get("/generateraceAPI", (req, res) => {
   let hash = helper.generateNewHash();
-  redisClient.get(hash, (err, room) => {
+  raceRooms.get(hash, (err, room) => {
     if (room == null) {
       data = {
         hash: hash,
@@ -41,9 +47,9 @@ io.on("connection", (socket) => {
   console.log("A user is connected");
 
   socket.on("disconnect", () => {
-    redisClient.keys("*", (err, rooms) => {
+    raceRooms.keys("*", (err, rooms) => {
       for (let i = 0; i < rooms.length; i++) {
-        redisClient.get(rooms[i], (err, userintheroom) => {
+        raceRooms.get(rooms[i], (err, userintheroom) => {
           json_data = JSON.parse(userintheroom);
           for (let j = 0; j < json_data.length; j++) {
             if (socket.id == json_data[j].socketId) {
@@ -52,7 +58,7 @@ io.on("connection", (socket) => {
                 `${json_data[j].name} has left the race`
               );
               json_data.splice(j, 1);
-              redisClient.set(rooms[i], JSON.stringify(json_data));
+              raceRooms.set(rooms[i], JSON.stringify(json_data));
             }
           }
         });
@@ -76,18 +82,16 @@ io.on("connection", (socket) => {
         "You have already joined the room!"
       );
     } else {
-      return aget(room)
+      return syncraceget(room)
         .then((res) => {
           if (res != null) {
             res = JSON.parse(res);
-            time = Math.floor(Date.now() / 1000);
-            diff = time - res[0];
-            if (diff < raceStartingTime) {
+            if (res[0] != true) {
               userDetails = {};
               userDetails.name = username;
               userDetails.socketId = socket.id;
               res.push(userDetails);
-              redisClient.set(room, JSON.stringify(res), "EX", expiryInSeconds);
+              raceRooms.set(room, JSON.stringify(res));
               return;
             } else {
               io.to(socket.id).emit("RACE_STARTED", "Race has already started");
@@ -98,7 +102,7 @@ io.on("connection", (socket) => {
             userDetails = {};
             userDetails.name = username;
             userDetails.socketId = socket.id;
-            redisClient.set(
+            raceRooms.set(
               room,
               JSON.stringify([time, userDetails]),
               "EX",
@@ -117,11 +121,15 @@ io.on("connection", (socket) => {
             //     console.log(client);
             //   });
 
-            redisClient.get(room, (err, res) => {
+            raceRooms.get(room, (err, res) => {
               res = JSON.parse(res);
-              usernames = res.map((userData) => {
-                return userData.name;
-              });
+              usernames = res
+                .map((userData) => {
+                  if (userData.name != undefined) {
+                    return userData.name;
+                  }
+                })
+                .filter((data) => data != null);
 
               let serverData = {
                 room: room,
@@ -129,7 +137,7 @@ io.on("connection", (socket) => {
               };
               console.log("Joined");
 
-              io.to(room).emit("userjoined", serverData);
+              io.in(room).emit("USER_JOINED", serverData);
             });
           }
         })
@@ -137,6 +145,54 @@ io.on("connection", (socket) => {
           console.log(err);
         });
     }
+  });
+
+  socket.on("START_RACE", ({ room }) => {
+    startRace = true;
+    let paraTypedByTheUserInTheRoom = [];
+
+    return syncraceget(room)
+      .then((res) => {
+        return new Promise((resolve) => {
+          jsonData = JSON.parse(res);
+          console.log(jsonData[0]);
+          jsonData[0] != true ? jsonData.unshift(true) : null;
+          raceRooms.set(room, JSON.stringify(jsonData));
+          for (userDetails of jsonData) {
+            if (userDetails.name != undefined) {
+              paraTypedByUser.get(userDetails.name, (err, res) => {
+                if (res == null) {
+                  paraTypedByUser.set(
+                    userDetails.name,
+                    JSON.stringify([1, 2, 3])
+                  );
+                  resolve();
+                } else {
+                  res = JSON.parse(res);
+                  paraTypedByTheUserInTheRoom.push(...res);
+                  resolve();
+                }
+              });
+            }
+          }
+        });
+      })
+      .then((_) => {
+        request.get(
+          {
+            url: TFSBackendURL,
+            qs: { data: JSON.stringify(paraTypedByTheUserInTheRoom) },
+          },
+          (err, res, body) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(res.body);
+              io.in(room).emit("USER_JOINED", res.body);
+            }
+          }
+        );
+      });
   });
 
   socket.on("Typing", ({ room, message }) => {
