9532633ec4ed2d2cf78b221917977f67064643f1
TheMightyRaider <sivasanjay7@gmail.com>
2020-06-30 14:15:53 +0530

    Added test for joinroom socketevent

--- __tests__/socketEvents.js
+++ __tests__/socketEvents.js
@@ -1,13 +1,21 @@
 const ioClient = require("socket.io-client");
 const { io, server } = require("../utils");
 const socketEvents = require("../socketEvent");
+const redis = require("../redisHelper");
+const redisMock = require("redis-mock");
 
 let socket;
 let responseSocket = null;
 
+const r = redisMock.createClient();
+
+const wait = (ms) => {
+  return new Promise((res) => setTimeout(res, ms));
+};
+
 beforeAll((done) => {
   io;
-  server.listen(4000, () => {
+  server.listen(3000, () => {
     console.log("Running!");
   });
   done();
@@ -15,11 +23,12 @@ beforeAll((done) => {
 
 afterAll((done) => {
   io.close();
+  server.close();
   done();
 });
 
 beforeEach((done) => {
-  socket = ioClient.connect(`http://localhost:4000`, {
+  socket = ioClient.connect(`http://localhost:3000`, {
     "reconnection delay": 0,
     "reopen delay": 0,
     "force new connection": true,
@@ -39,6 +48,7 @@ afterEach((done) => {
   if (socket.connected) {
     socket.disconnect();
   }
+  r.flushall();
   done();
 });
 
@@ -50,13 +60,72 @@ describe("Socket Test ! ", () => {
       done();
     });
   });
+});
 
-  it("createorjoinroom -> case #1, When room doesn't exists", (done) => {
+describe("Test for various scenario for createorjoinroom socket event", () => {
+  it("case #1 -> When room doesn't exists", async (done) => {
     socket.emit("create/join", { username: "userOne", room: "zeta" });
     socketEvents.createorjoinroom(responseSocket);
-
     socket.on("USER_JOINED", (msg) => {
       expect(msg).toBe("Room doesn't exists");
+      expect(
+        io.sockets.adapter.sids[responseSocket.id]["room-zeta"]
+      ).toBeFalsy();
+      done();
+    });
+  });
+
+  it("case #2 -> Adding a user to a room", (done) => {
+    redis.set(
+      "room-eplison",
+      JSON.stringify({ timestamp: Math.floor(Date.now() / 1000), users: [] })
+    );
+    socket.emit("create/join", { username: "userOne", room: "eplison" });
+    socketEvents.createorjoinroom(responseSocket);
+    socket.on("USER_JOINED", (msg) => {
+      expect(msg).toStrictEqual({
+        room: "room-eplison",
+        userInTheRoom: ["userOne"],
+      });
+      expect(
+        io.sockets.adapter.sids[responseSocket.id]["room-eplison"]
+      ).toBeTruthy();
+      done();
+    });
+  });
+
+  it("case #3 -> Not adding users when the race has started for a room", (done) => {
+    redis.set(
+      "room-zeta",
+      JSON.stringify({
+        isStarted: true,
+        timestamp: Math.floor(Date.now() / 1000),
+        users: [],
+      })
+    );
+    socket.emit("create/join", { username: "userOne", room: "zeta" });
+    socketEvents.createorjoinroom(responseSocket);
+    socket.on("RACE_STARTED", (msg) => {
+      expect(msg).toBe("Race has already started");
+      expect(
+        io.sockets.adapter.sids[responseSocket.id]["room-zeta"]
+      ).toBeFalsy();
+      done();
+    });
+  });
+  it("case #4 -> Not adding a user, when the user is already added to the room", async (done) => {
+    redis.set(
+      "room-alpha",
+      JSON.stringify({
+        timestamp: Math.floor(Date.now() / 1000),
+        users: [],
+      })
+    );
+    socket.emit("create/join", { username: "userOne", room: "alpha" });
+    socket.emit("create/join", { username: "userTwo", room: "alpha" });
+    socketEvents.createorjoinroom(responseSocket);
+    socket.on("ALREADY_JOINED", (msg) => {
+      expect(msg).toBe("You have already joined the room!");
       done();
     });
   });
