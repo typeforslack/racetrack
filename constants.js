d217f03686be0215b3a51c70cd3442c535fa459e
TheMightyRaider <sivasanjay7@gmail.com>
2020-08-10 22:22:00 +0530

    Updated para url

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const PORT = 4000;
 const expiryInSeconds = 3600;
-const TFSBackendURL = "http://127.0.0.1:8000/paraForRaceTrack";
+const TFSBackendURL = "http://127.0.0.1:8000/internal/racetrack/para";
 
 module.exports = {
   PORT,
